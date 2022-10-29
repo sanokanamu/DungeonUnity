266e101123e813f4686dcc1025b115ab2ce644bd
kannaduki2000 <ryou10121012@icloud.com>
2022-10-29 16:16:54 +0900

    sanokanamu
    アニメーションをつけました

--- Assets/Scripts/DungeonManager.cs
+++ Assets/Scripts/DungeonManager.cs
@@ -70,6 +70,10 @@ public class DungeonManager : MonoBehaviour
         },
     };
 
+    [SerializeField, Header("プレイヤーをアタッチする")]
+
+    private PlayerView _playerView = null;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -79,10 +83,10 @@ public class DungeonManager : MonoBehaviour
     private void MapMake()
     {
         //  y を０から１９まで変化させる
-        foreach (int y in Enumerable.Range(0,20))
+        foreach (int y in Enumerable.Range(0, 20))
         {
             //  x を０から１９まで変化させる
-            foreach (int x in Enumerable.Range(0,20))
+            foreach (int x in Enumerable.Range(0, 20))
             {
                 //  プレハブの実態をヒエラルキーに生成する
                 GameObject gobj = Instantiate(_mapParts, _parent);
@@ -90,8 +94,32 @@ public class DungeonManager : MonoBehaviour
                 gobj.transform.localPosition = new Vector3(-304 + x * 32, 304 - y * 32, 0);
                 //  マップスプライトの設定
                 // gobj.GetComponent<ChipView>().SetImage(_mapChipSprites[_mapData[0][y][x]]);
-                gobj.GetComponent<ChipView>().SetImage(_mapChipSprites[_mapDataList[0,y,x]]);
+                gobj.GetComponent<ChipView>().SetImage(_mapChipSprites[_mapDataList[0, y, x]]);
             }
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            _playerView.WalkAction(PlayerView.PlayerDirection.Right);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            _playerView.WalkAction(PlayerView.PlayerDirection.Left);
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            _playerView.WalkAction(PlayerView.PlayerDirection.Back);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            _playerView.WalkAction(PlayerView.PlayerDirection.Front);
+        }
+        else if(false == _playerView.IsWalking)
+        {
+            _playerView.SetAnimationState(PlayerView.PlayerMode.Idle);
         }
     }
 }
--- Assets/Scripts/PlayerView.cs
+++ Assets/Scripts/PlayerView.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerView : MonoBehaviour
 {
     //  アニメーションに関連あるプレイヤーの状態
-    private enum PlayerMode
+    public enum PlayerMode
     {
         Idle = 0,
         RightWalk = 1,
@@ -13,18 +13,103 @@ public class PlayerView : MonoBehaviour
         LeftWalk = 3,
         BackWalk = 4
     }
+
+    public enum PlayerDirection
+    {
+        None = 0,
+        Right = 1,
+        Front = 2,
+        Left = 3,
+        Back = 4
+    }
     //  プレイヤーの状態
     private PlayerMode _playerMode = PlayerMode.Idle;
-    
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField]
+    private Animator _animator = null;
+
+    private Vector3 _ofp = Vector3.zero;
+
+    private Dictionary<PlayerDirection, Vector3> _playerWalkAddLists = new Dictionary<PlayerDirection, Vector3>()
     {
-        
+        {PlayerDirection.None , Vector3.zero},
+        {PlayerDirection.Right , new Vector3(x:1f, y:0f, z:0f)},
+        {PlayerDirection.Front , new Vector3(x:0f, y:-1f, z:0f)},
+        {PlayerDirection.Left , new Vector3(x:-1f, y:0f, z:0f)},
+        {PlayerDirection.Back , new Vector3(x:0f, y:1f, z:0f)},
+    };
+    // 移動速度
+    [SerializeField]
+    private float _walkSpeed = 1f;
+
+    private bool _isWalking = false;
+    public bool IsWalking => _isWalking;
+
+    public Vector2 PlayerPos { get; private set; } = new Vector2(x: 1, y: 1);
+
+    private static readonly int PlayerAnimStat = Animator.StringToHash("PlayerAnimStat");
+
+    private static readonly Vector3 PlayerInitialposition = new Vector3(x: -272f, y: 272f, z: 0f);
+
+    private void Awake()
+    {
+        transform.localPosition = PlayerInitialposition;
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// 外部よりアニメーションのモードを切り替える
+    /// </summary>
+    /// <param name="playerMode"></param>
+
+    // Start is called before the first frame update
+    public void SetAnimationState(PlayerMode playerMode)
     {
-        
+        _animator.SetInteger("PlayerAnimStat", (int)playerMode);
     }
+
+    /// <summary>
+    /// 移動処理
+    /// </summary>
+    public void WalkAction(PlayerDirection playerDirection)
+    {
+        //すでに移動中ならば何もしない
+        if (_isWalking) return;
+        _isWalking = true;
+        //移動開始
+        StartCoroutine(Walking(playerDirection));
+    }
+
+    /// <summary>
+    /// 移動のルーチン
+    /// </summary>
+    /// <param name="playerDirection"></param>
+    /// <returns></returns>
+    private IEnumerator Walking(PlayerDirection playerDirection)
+    {
+        //移動前の座標
+        Vector3 orgPos = transform.localPosition;
+        _ofp = Vector3.zero;
+        SetAnimationState((PlayerMode)playerDirection);
+        while (true)
+        {
+            //加算する座標を計算する
+            _ofp += _playerWalkAddLists[playerDirection] * _walkSpeed;
+            // 移動範囲が1ブロックを超えたら、1ブロック分に直す
+            if (_ofp.magnitude > 32f)
+            {
+                _ofp = _playerWalkAddLists[playerDirection] * 32;
+                //座標を更新する
+                transform.localPosition = orgPos + _ofp;
+                //ループを抜ける
+                break;
+            }
+            //座標を更新する
+            transform.localPosition = orgPos + _ofp;
+
+            //次の updote まで待つ
+            yield return null;
+        }
+        _isWalking = false;
+    }
+
 }
