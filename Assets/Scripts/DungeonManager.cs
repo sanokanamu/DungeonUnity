2480520d9f8a22f3f24762e13c3341bd15dd9104
sanokanamu <104252300+sanokanamu@users.noreply.github.com>
2022-11-05 12:46:46 +0900

    sano
    先生データに変更

--- Assets/Scripts/DungeonManager.cs
+++ Assets/Scripts/DungeonManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,18 +24,18 @@ public class DungeonManager : MonoBehaviour
         //   0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 
             {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
             {1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-            {1,0,1,1,1,1,0,0,0x0107,1,1,1,1,1,1,1,1,1,1,1},
-            {1,0,1,1,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1},
-            {1,0,1,1,0,1,0,0,0,1,1,1,1,1,1,1,1,1,1,1},
-            {1,0,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-            {1,0,1,1,0,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1},
-            {1,0,1,1,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1},
-            {1,0,1,1,1,1,1,0,1,1,1,1,0,1,1,1,1,1,1,1},
-            {1,0,1,1,1,1,1,0,1,1,1,1,0,1,1,1,1,1,1,1},
-            {1,0,1,1,1,1,1,0,1,1,1,1,0,1,1,1,1,1,1,1},
-            {1,0,1,1,1,1,1,0,1,1,1,1,0,0,0,0,1,1,1,1},
-            {1,0,0,0,0,0,0,0,0x0104,1,1,1,1,0,0,0,1,1,1,1},
-            {1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0x0109,1,1,1,1},
+            {1,0,1,1,1,1,1,1,1,0x0107,0,0,1,1,1,1,1,1,1,1},
+            {1,0,1,1,1,1,1,1,1,0,0,0,1,1,1,1,1,1,1,1},
+            {1,0,1,1,1,1,1,1,1,0,0,0x1009,1,1,1,1,1,1,1,1},
+            {1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+            {1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+            {1,0,1,1,2,0,0,0,0,0,0,0,0x0109,1,1,1,1,1,1,1},
+            {1,0,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1},
+            {1,0,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1},
+            {1,0,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1},
+            {1,0,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1},
+            {1,0,0,0,0,0,0,0,0x0104,1,1,1,1,1,1,1,1,1,1,1},
+            {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
             {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
             {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
             {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
@@ -42,7 +43,7 @@ public class DungeonManager : MonoBehaviour
             {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
             {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
         },
-         {
+        {
         //   0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 
             {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
             {1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
@@ -50,8 +51,8 @@ public class DungeonManager : MonoBehaviour
             {1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
             {1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
             {1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-            {1,0,1,1,1,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1},
-            {1,0,1,1,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1},
+            {1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+            {1,0,1,1,3,0,0,0,0,0,0,0,6,1,1,1,1,1,1,1},
             {1,0,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1},
             {1,0,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1},
             {1,0,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1},
@@ -67,11 +68,10 @@ public class DungeonManager : MonoBehaviour
         },
     };
 
-
     [SerializeField, Header("プレイヤーをアタッチする")]
-
     private PlayerView _playerView = null;
 
+    //  当たり判定のマップキャラクター番号
     private List<int> _mapHitTable = new List<int>() { 1, 4, 7, 8 };
 
     // Start is called before the first frame update
@@ -83,51 +83,58 @@ public class DungeonManager : MonoBehaviour
     /// <summary>
     /// マップデータの取得
     /// </summary>
-    /// <param name="pos"></param>
-    /// <returns></returns>
+    /// <param name="pos">移動先座標</param>
+    /// <returns>マップデータ</returns>
     private int GetMapData(Vector3Int pos)
     {
         return _mapDataList[0, pos.y, pos.x] & 0xff;
     }
 
+    /// <summary>
+    /// マップステータス
+    /// </summary>
+    /// <param name="pos">移動先座標</param>
+    /// <returns>マップ属性</returns>
     private int GetMapStat(Vector3Int pos)
     {
         return _mapDataList[0, pos.y, pos.x] >> 8;
     }
-
+    /// <summary>
+    /// 移動可能な場所かどうかのチェック
+    /// </summary>
+    /// <param name="pos"></param>
+    /// <returns></returns>
     private bool IsWalkEnable(Vector3Int pos)
     {
-        int mapData = GetMapData(pos);      // マップデータの取得
-        int mapStat = GetMapStat(pos);      // マップ情報の取得
-        // マップが壁であり、通り抜け可能なら true を返す
+        int mapData = GetMapData(pos);      //  マップデータの取得
+        int mapStat = GetMapStat(pos);      //  マップ情報の取得
+        //  マップが壁であり、通り抜け可能なら true を返す
         if (1 == mapStat && 1 == mapData)
             return true;
-        // 当たり判定のチップデータを順番にマップデータと比較してヒットしたらr移動不可能なので false を返す
+        //  当たり判定のチップデータを順番にマップデータと比較してヒットしたら移動不可なので false を返す
         foreach (int mapHitChip in _mapHitTable)
         {
             if (mapHitChip == mapData)
                 return false;
         }
-        // どのチップとも一致しなければ通り抜け可能なので true を返す
+        //  どのチップとも一致しなければ通り抜け可能なので true を返す
         return true;
     }
-
-
+    
     private void MapMake()
     {
         //  y を０から１９まで変化させる
-        foreach (int y in Enumerable.Range(0, 20))
+        foreach (int y in Enumerable.Range(0,20))
         {
             //  x を０から１９まで変化させる
-            foreach (int x in Enumerable.Range(0, 20))
+            foreach (int x in Enumerable.Range(0,20))
             {
                 //  プレハブの実態をヒエラルキーに生成する
                 GameObject gobj = Instantiate(_mapParts, _parent);
                 //  表示座標を設定する
                 gobj.transform.localPosition = new Vector3(-304 + x * 32, 304 - y * 32, 0);
                 //  マップスプライトの設定
-                // gobj.GetComponent<ChipView>().SetImage(_mapChipSprites[_mapData[0][y][x]]);
-                gobj.GetComponent<ChipView>().SetImage(_mapChipSprites[_mapDataList[0, y, x]]);
+                gobj.GetComponent<ChipView>().SetImage(_mapChipSprites[_mapDataList[0,y,x]]);
             }
         }
     }
@@ -136,27 +143,25 @@ public class DungeonManager : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            if (IsWalkEnable(_playerView.GetNextPosition(PlayerView.PlayerDirection.Right)))
+            if(IsWalkEnable(_playerView.GetNextPosition(PlayerView.PlayerDirection.Right)))
                 _playerView.WalkAction(PlayerView.PlayerDirection.Right);
         }
         else if (Input.GetKey(KeyCode.LeftArrow))
         {
-            if (IsWalkEnable(_playerView.GetNextPosition(PlayerView.PlayerDirection.Left)))
+            if(IsWalkEnable(_playerView.GetNextPosition(PlayerView.PlayerDirection.Left)))
                 _playerView.WalkAction(PlayerView.PlayerDirection.Left);
         }
         else if (Input.GetKey(KeyCode.UpArrow))
         {
-            if (IsWalkEnable(_playerView.GetNextPosition(PlayerView.PlayerDirection.Back)))
+            if(IsWalkEnable(_playerView.GetNextPosition(PlayerView.PlayerDirection.Back)))
                 _playerView.WalkAction(PlayerView.PlayerDirection.Back);
         }
         else if (Input.GetKey(KeyCode.DownArrow))
         {
-            if (IsWalkEnable(_playerView.GetNextPosition(PlayerView.PlayerDirection.Front)))
+            if(IsWalkEnable(_playerView.GetNextPosition(PlayerView.PlayerDirection.Front)))
                 _playerView.WalkAction(PlayerView.PlayerDirection.Front);
         }
-        else if (false == _playerView.IsWalking)
-        {
+        else if(false == _playerView.IsWalking)
             _playerView.SetAnimationState(PlayerView.PlayerMode.Idle);
-        }
     }
 }
--- Assets/Scripts/PlayerView.cs
+++ Assets/Scripts/PlayerView.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerView : MonoBehaviour
@@ -14,6 +16,7 @@ public class PlayerView : MonoBehaviour
         BackWalk = 4
     }
 
+    //  プレイヤーの向き
     public enum PlayerDirection
     {
         None = 0,
@@ -25,112 +28,111 @@ public class PlayerView : MonoBehaviour
     //  プレイヤーの状態
     private PlayerMode _playerMode = PlayerMode.Idle;
 
+    //  プレイヤーアニメーションのアニメーターを設定
     [SerializeField]
     private Animator _animator = null;
-
+    
     private Vector3 _ofp = Vector3.zero;
 
     private Dictionary<PlayerDirection, Vector3> _playerWalkAddLists = new Dictionary<PlayerDirection, Vector3>()
     {
-        {PlayerDirection.None , Vector3.zero},
-        {PlayerDirection.Right , new Vector3(1f, 0f, 0f)},
-        {PlayerDirection.Front , new Vector3(0f, -1f, 0f)},
-        {PlayerDirection.Left , new Vector3(-1f, 0f, 0f)},
-        {PlayerDirection.Back , new Vector3(0f, 1f, 0f)},
+        { PlayerDirection.None , Vector3.zero}, 
+        { PlayerDirection.Right, new Vector3(1f, 0f, 0f) },
+        { PlayerDirection.Front, new Vector3(0f, -1f, 0f) },
+        { PlayerDirection.Left, new Vector3(-1f, 0f, 0f) },
+        { PlayerDirection.Back, new Vector3(0f, 1f, 0f) },
     };
     private Dictionary<PlayerDirection, Vector3Int> _playerPosAddLists = new Dictionary<PlayerDirection, Vector3Int>()
     {
-        {PlayerDirection.None , Vector3Int.zero},
-        {PlayerDirection.Right , new Vector3Int(1, 0, 0)},
-        {PlayerDirection.Front , new Vector3Int(0, 1, 0)},
-        {PlayerDirection.Left , new Vector3Int(-1, 0, 0)},
-        {PlayerDirection.Back , new Vector3Int(0, -1, 0)},
+        { PlayerDirection.None , Vector3Int.zero}, 
+        { PlayerDirection.Right, new Vector3Int(1, 0, 0) },
+        { PlayerDirection.Front, new Vector3Int(0, 1, 0) },
+        { PlayerDirection.Left, new Vector3Int(-1, 0, 0) },
+        { PlayerDirection.Back, new Vector3Int(0, -1, 0) },
     };
-    // 移動速度
+    //  移動速度
     [SerializeField]
     private float _walkSpeed = 1f;
-
+    //  移動中かどうかの判定フラグ
     private bool _isWalking = false;
-    public bool IsWalking => _isWalking;
-
+    public  bool IsWalking => _isWalking;
+    
+    //  プレイヤー初期座標
     public Vector3Int PlayerPos { get; private set; } = new Vector3Int(1, 1, 0);
 
-    private static readonly int PlayerAnimStat = Animator.StringToHash("PlayerAnimStat");
-
-    private static readonly Vector3 PlayerInitialposition = new Vector3(-272f, 272f, 0f);
+    private static readonly int     PlayerAnimStat        = Animator.StringToHash("PlayerAnimStat");
+    private static readonly Vector3 PlayerInitialPosition = new Vector3(-272f, 272f, 0f);
 
     private void Awake()
     {
-        transform.localPosition = PlayerInitialposition;
+        //  プレイヤーの初期座標を指定する
+        transform.localPosition = PlayerInitialPosition;
     }
 
     /// <summary>
     /// 外部よりアニメーションのモードを切り替える
     /// </summary>
-    /// <param name="playerMode"></param>
-
-    // Start is called before the first frame update
+    /// <param name="playerMode">アニメーションの状態</param>
     public void SetAnimationState(PlayerMode playerMode)
     {
-        _animator.SetInteger("PlayerAnimStat", (int)playerMode);
+        _animator.SetInteger(PlayerAnimStat, (int)playerMode);
     }
 
     /// <summary>
     /// 移動処理
     /// </summary>
+    /// <param name="playerDirection">移動方向</param>
     public void WalkAction(PlayerDirection playerDirection)
     {
-        //すでに移動中ならば何もしない
+        //  すでに移動中ならば何もしない
         if (_isWalking) return;
         _isWalking = true;
-        //移動開始
+        SetAnimationState((PlayerMode)playerDirection);
+        //  移動開始
         StartCoroutine(Walking(playerDirection));
     }
+
     /// <summary>
     /// プレイヤーの次の目的地（座標）を取得する
-    /// ** 実際には移動しない。移動確認用に使用する
+    /// ** 実際には移動はしない。移動確認用に使用する。
     /// </summary>
-    /// <param name="playerDirection"></param>
-    /// <returns></returns>
+    /// <param name="playerDirection">移動方向</param>
+    /// <returns>移動予定の場所</returns>
     public Vector3Int GetNextPosition(PlayerDirection playerDirection)
     {
         return (PlayerPos + _playerPosAddLists[playerDirection]);
     }
-
+    
     /// <summary>
     /// 移動のコルーチン
     /// </summary>
-    /// <param name="playerDirection"></param>
+    /// <param name="playerDirection">移動方向</param>
     /// <returns></returns>
     private IEnumerator Walking(PlayerDirection playerDirection)
     {
-        //キャラクター座標の更新
+        //  キャラクター座標の更新
         PlayerPos += _playerPosAddLists[playerDirection];
-
-        //移動前の座標
+        //  移動前の座標
         Vector3 orgPos = transform.localPosition;
         _ofp = Vector3.zero;
-        SetAnimationState((PlayerMode)playerDirection);
         while (true)
         {
-            //加算する座標を計算する
-            _ofp += _playerWalkAddLists[playerDirection] * _walkSpeed;
-            // 移動範囲が1ブロックを超えたら、1ブロック分に直す
+            //  加算する座標を計算する
+            _ofp                    += _playerWalkAddLists[playerDirection] * _walkSpeed;
+            //  移動範囲が１ブロックを超えたら、１ブロック分に直す
             if (_ofp.magnitude > 32f)
             {
-                _ofp = _playerWalkAddLists[playerDirection] * 32;
-                //座標を更新する
+                _ofp                    = _playerWalkAddLists[playerDirection] * 32;
+                //  座標を更新する
                 transform.localPosition = orgPos + _ofp;
-                //ループを抜ける
+                //  ループを抜ける
                 break;
             }
-            //座標を更新する
-            transform.localPosition = orgPos + _ofp;
-
-            //次の updote まで待つ
+            //  座標を更新する
+            transform.localPosition =  orgPos + _ofp;
+            //  次の update まで待つ
             yield return null;
         }
         _isWalking = false;
     }
-
 }
